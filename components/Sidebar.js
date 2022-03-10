4a5d48f49f51dc63f97e03b11f49aa1a639d0280
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-03-10 21:00:00 +0100

    Base Created

---
+++ components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Sidebar() {
+  return (
+    <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
+
+        <div className='flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24'>
+            <Image src='https://rb.gy/ogau5a' width={30} height={30}/> 
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
