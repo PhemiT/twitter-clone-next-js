23888c6004d1b4f7da5f7428de121a2fe230530a
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-03-12 13:10:58 +0100

    Sidebar Created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import SidebarLink from './SidebarLink'
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from '@heroicons/react/outline'
+import {HomeIcon} from '@heroicons/react/solid'
 
 function Sidebar() {
   return (
@@ -9,6 +21,16 @@ function Sidebar() {
             <Image src='https://rb.gy/ogau5a' width={30} height={30}/> 
         </div>
 
+        <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+          <SidebarLink text='Home' Icon={HomeIcon} active />
+          <SidebarLink text='Explore' Icon={HashtagIcon} />
+          <SidebarLink text='Notifications' Icon={BellIcon} />
+          <SidebarLink text='Messages' Icon={InboxIcon} />
+          <SidebarLink text='Bookmarks' Icon={BookmarkIcon} />
+          <SidebarLink text='Lists' Icon={ClipboardListIcon} />
+          <SidebarLink text='Profile' Icon={UserIcon} />
+          <SidebarLink text='More' Icon={DotsCircleHorizontalIcon} />
+        </div>
     </div>
   )
 }
