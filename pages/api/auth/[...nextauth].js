572579e0c466a19a9862d7f7a68600afb6126c32
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-03-14 15:08:53 +0100

    Authentication

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export default NextAuth({
+
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      }),
+  ],
+
+  callbacks: {
+      async session({session,token}) {
+          session.user.tag = session.user.name.split(' ').join('').toLocaleLowerCase()
+
+          session.user.uid = token.sub
+          return session
+      },
+  },
+})
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
-import styles from '../styles/Home.module.css'
+import {getProviders, getSession, useSession} from 'next-auth/react'
 
 export default function Home() {
   return (
@@ -19,4 +19,25 @@ export default function Home() {
 
     </div>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
