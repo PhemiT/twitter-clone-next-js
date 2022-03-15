0a4fb1fe1ae51b39a874c1dfae965f66efa8665e
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-03-15 22:59:26 +0100

    Post Base

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,25 @@
-import { SparklesIcon } from '@heroicons/react/outline'
-import React from 'react'
-import Input from './Input'
+import { SparklesIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+import Input from "./Input";
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
+import { useSession } from "next-auth/react";
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className='flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
         <div className='text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700'>
@@ -13,6 +30,11 @@ function Feed() {
         </div>
 
         <Input />
+        <div className='pb-72'>
+          {posts.map(post =>(
+            <Post key='' id='' post={post.data} />
+          ))}
+        </div>
     </div>
   )
 }
